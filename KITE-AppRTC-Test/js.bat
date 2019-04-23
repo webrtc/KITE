4b599c585bca9ad9ce4a62d19e870d17e3a171fc
ManuCosmo <emmanuel.andre@cosmosoftware.io>
2019-04-23 17:42:58 +0800

    delete js.bat (replaced by command r)

--- KITE-AppRTC-Test/js.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-java -cp "../KITE-Engine/target/*;../KITE-Extras/target/*;../KITE-Framework/target/*;../KITE-Engine/target/*;target/*" org.webrtc.kite.Engine configs/js.iceconnection.local.config.json
